1c28bee18a0f25ea1e4c7d7569822e69345ac5aa
Igor Vieira <higorc2@gmail.com>
2016-11-17 16:37:58 -0200

    Add Assignment 2

---
+++ week-2/assignment/models/leadership.js
@@ -0,0 +1,34 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema  = mongoose.Schema;
+
+// create a schema
+var leaderSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  designation: {
+    type: String,
+    required: true
+  },
+  abbr: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+});
+
+// create a model
+var Leaders = mongoose.model('Leader', leaderSchema);
+
+// make this available to our Node applications
+module.exports = Leaders;
